263ff5386310bde483ae583667a925d7fa6ab11e
Alejandro Medici <alejandro.medici@nextiteration.com.ar>
2021-12-02 20:21:31 -0300

    ready for connect with DB

--- backendProject/settings.py
+++ backendProject/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'backendProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django',
+        'USER': 'userdjango',
+        'PASSWORD': 'passworddjango',
+        'HOST': 'db',
+        'PORT': 3306,
     }
 }
 
