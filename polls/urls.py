61132f7c5f59f689b5739b762ce551d384b3e527
Alejandro Medici <alejandro.medici@nextiteration.com.ar>
2021-12-07 20:37:14 -0300

    Project running with model

---
+++ polls/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [ 
+    path('', views.index, name='index'),
+    path('<int:poll_id>/', views.detail, name='detail'),
+    path('<int:poll_id>/results/', views.results, name='results'),
+]
--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+def detail(request, poll_id):
+    return HttpResponse("You're looking at poll %s." % poll_id)
+
+def results(request, poll_id):
+    return HttpResponse("You're looking at the results of poll %s." % poll_id)
